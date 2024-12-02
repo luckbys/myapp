4b73f56fddb5b4534f5ce8b860314c66f3b3c48b
lacca.shop <lacca.shop@gmail.com>
2024-12-02 23:50:38 +0000

    teste flutter

---
+++ lib/main.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'My app',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+        
+      ),
+      home: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Stack(
+            alignment: AlignmentDirectional.center,
+            children: [
+              Container(
+                color: Colors.white,
+                width: 100,
+                height: 100,
+              ),
+              Container(
+                color: Colors.blue,
+                width: 50,
+                height: 50,
+              )
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
